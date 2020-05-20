3d76686b9a09d54ea6937ca85f288d2e71140f3a
Sarah C. Conner <35582378+s-conner@users.noreply.github.com>
2020-05-20 12:30:10 -0400

    Updated the readme and added an R program

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # repo_for_ari
+
+# Hello Ariel! We should do a bingo card themed after the Office.
---
+++ hello.R
@@ -0,0 +1,6 @@
+# Hello, Ari! Excited to play some bingo today.
+
+# Here is a popular R package for RMST, maybe it will be of use for your next project!
+
+install.packages('survRM2')
+library(survRM2)
